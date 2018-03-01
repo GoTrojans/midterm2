051edb8b2dab7c05dc0c86314aa62247837f478c
Zhao Zhao <zhao329@usc.edu>
2018-02-28 17:34:13 -0800

    Initial Commit

---
+++ Assets/Enemy/watergrass/GrassBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassBehavior : MonoBehaviour {
+
+    int timeCounter;
+    int runHash = Animator.StringToHash("run");
+    int randHash = Animator.StringToHash("rand_start");
+    // set some as real Enemy and some as plain watergrass
+    [SerializeField] bool isRealEnemy = true;
+    float randomStart;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        timeCounter = 0;
+        anim = GetComponent<Animator>();
+        randomStart =  Random.Range(0, 100f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        randomStart--;
+
+        //random animation
+        if (randomStart < 0) {
+            anim.SetTrigger(randHash);
+        }
+
+        timeCounter++;
+        if (timeCounter >= 10 && isRealEnemy) {
+            // do Attack movement
+            anim.SetTrigger(runHash);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    // todo work-out why sometimes slow on first play of scene
+
+    [Header("General")]
+    [Tooltip("In ms^-1")][SerializeField] float controlSpeed= 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+    [SerializeField] GameObject[] guns;
+    [SerializeField] GameObject specialGun;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5f;
+
+    [Header("Control-throw Based")]
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    [Header("Special Gun")][SerializeField]float specialGunTime = 0.5f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    private void Start()
+    {
+        stopSpecialGun();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+            ProcessFiring();
+        }
+    }
+
+    void OnPlayerDeath() // called by string reference
+    {
+        isControlEnabled = false;
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessFiring()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire"))
+        {
+            SetGunsActive(true);
+        }
+        else
+        {
+            SetGunsActive(false);
+        }
+
+        var emissionModule = specialGun.GetComponent<ParticleSystem>().emission;
+
+        if (CrossPlatformInputManager.GetButton("Special Fire")) {
+            
+            emissionModule.enabled = true;
+
+            Invoke("stopSpecialGun", specialGunTime);
+        }
+
+
+
+
+    }
+
+    void stopSpecialGun() {
+        var emissionModule = specialGun.GetComponent<ParticleSystem>().emission;
+        emissionModule.enabled = false;
+    }
+
+    private void SetGunsActive(bool isActive)
+    {
+        foreach (GameObject gun in guns) // care may affect death FX
+        {
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
+    }
+}
